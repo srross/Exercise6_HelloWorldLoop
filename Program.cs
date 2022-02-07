6170ca6a13a2408c91f454a741fbe998770cff25
SoniaRoss <srross99@gmail.com>
2022-02-07 16:05:57 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Loops_exercise6
+{
+    public class Program
+
+    {
+        public static void Main(string[] args)
+
+        {
+            string userInput;
+            bool repeatLoop = true;
+
+            do
+            {
+                Console.WriteLine("Hello World!");
+
+                Console.Write("Do you want to continue? (yes/no): ");
+
+                userInput = Console.ReadLine().ToLower();
+
+                if (userInput != "y" && userInput != "yes")
+                {
+                    repeatLoop = false;
+                    Console.WriteLine("Bye-bye");
+                    break;
+                }
+            }
+            while (repeatLoop == true);
+        }
+    }
+}
